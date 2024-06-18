81310b56d6aa58ffbf77e0340f5a5f84baadaf58
omrani-mohamed <omrani.mohamedamine@esprit.tn>
2024-06-18 12:24:44 +0100

    4th commit

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardSkeleton from '@/app/ui/skeletons';
+export default function loading (){
+    return(
+        <div>
+            <DashboardSkeleton />
+        </div>
+    );
+
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,14 +3,17 @@ import Link from 'next/link';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 export default function Loginpage (){
+    
     return(
         <div className ={styles.container}>
-            <form action="" className={styles.form}>
+            <form className={styles.form}>
                 <h1>Login</h1>
                 <input type="text" placeholder='Username' />
                 <input type="email" placeholder='Email' />
                 <input type="password" placeholder='Password' />
-                {/* <button>Login</button> */}
+                {/* <button type= "submit" 
+                className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
+                <ArrowRightIcon className="w-5 md:w-6" />Log in</button> */}
                 <Link
                 href="/dashboard"
                 className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
@@ -20,3 +23,4 @@ export default function Loginpage (){
         </div>
     );
 }
+
